d88040ecfbb3c4eb9ebd35bf18ae4c2b3c3f65e4
Jonathan <jonathan@acss.net.au>
2011-02-22 08:27:54 +1000

    Update the init.rb to include patch magic.

--- init.rb
+++ init.rb
@@ -1,11 +1,18 @@
 require 'redmine'
 
+require 'dispatcher'
+
+Dispatcher.to_prepare :forward_to_diffs do
+    require_dependency 'application_helper'
+    ApplicationHelper.send(:include, DiffForwardPluginPatch) unless ApplicationHelper.included_modules.include?(DiffForwardPluginPatch)
+end
+
 Redmine::Plugin.register :forward_to_diffs do
-  name 'Forward to Diffs'
-  author 'Disqus (David Cramer)'
-  url 'http://github.com/dcramer/forward-to-diffs'
-  author_url 'http://www.disqus.com/'
-  description 'Forward to the diffs pane when viewing a revision.'
+  name 'Forward to diffs'
+  author 'Jon McManus'
+  url 'http://github.com/jmcb/forward-to-diffs'
+  author_url 'http://github.com/jmcb'
+  description 'Link users directly to diffs of revisions, rather than to the overview.'
   version '0.1'
 
   requires_redmine :version_or_higher => '0.8.0'
