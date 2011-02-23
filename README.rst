cefa887db785693c3aab6882fabcd4c71ace5502
Jonathan <jonathan@acss.net.au>
2011-02-23 10:31:21 +1000

    Fork forward-to-diffs into view-revision-tree.
    A very simple plug-in to allow linking directly to the source tree of a
    revision, rather than having to go to the "browse repository" and fill in the
    relevant box.

--- README.rst
+++ README.rst
@@ -1,8 +1,12 @@
-Rewrites commit links to point directly to revision rather than summary page,
-while allowing the user to go to the summary page at a later date.
+Provides a link in the "view revision" page to the complete tree
+for this commit.
 
 Licensed under the terms of the MIT license. See LICENSE.rst.
 
+Forked from `forward-to-diffs (mine)`_ on GitHub, by myself, which was:
+
 Forked from `forward-to-diffs`_ on GitHub, by dcramer.
+
+.. _`forward-to-diffs (mine)`: https://github.com/jmcb/forward-to-diffs
 
 .. _`forward-to-diffs`: https://github.com/dcramer/forward-to-diffs
--- init.rb
+++ init.rb
@@ -1,18 +1,11 @@
 require 'redmine'
 
-require 'dispatcher'
-
-Dispatcher.to_prepare :diff_forward do
-    require_dependency 'application_helper'
-    ApplicationHelper.send(:include, DiffForwardPluginPatch) unless ApplicationHelper.included_modules.include?(DiffForwardPluginPatch)
-end
-
-Redmine::Plugin.register :diff_forward do
-  name 'Forward to diffs'
+Redmine::Plugin.register :view_revision_tree do
+  name 'Links to view the complete source tree of a revision'
   author 'Jon McManus'
-  url 'http://github.com/jmcb/forward-to-diffs'
+  url 'http://github.com/jmcb/view-revision-tree'
   author_url 'http://github.com/jmcb'
-  description 'Link users directly to diffs of revisions, rather than to the overview.'
+  description 'Link to the entire source tree for this revision.'
   version '0.1'
 
   requires_redmine :version_or_higher => '0.8.0'
